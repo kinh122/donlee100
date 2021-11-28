e96c590ad5be5d48d20183019e50f24edabfeb5c
Don_Sflix <92365864+GYGGFDUSHSH@users.noreply.github.com>
2021-11-28 17:29:00 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 echo "Cloning Repo, Please Wait..."
-git clone -b https://github.com/GYGGFDUSHSH/donlee100.git /tgmoviebot
-cd /tgmoviebot
+git clone -b master https://github.com/GYGGFDUSHSH/donlee100.git /donlee100
+cd /donlee100
 echo "Installing Requirements..."
 pip3 install -U -r requirements.txt
 echo "Starting Bot, Please Wait..."
