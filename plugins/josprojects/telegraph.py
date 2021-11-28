94f9c6f7a291af8fbe1e2f7f9922f2e086820ff2
Don_Sflix <92365864+GYGGFDUSHSH@users.noreply.github.com>
2021-11-28 08:25:34 +0530

    Update telegraph.py

--- plugins/josprojects/telegraph.py
+++ plugins/josprojects/telegraph.py
@@ -57,9 +57,7 @@ async def telegraph(client, message):
             reply_markup=InlineKeyboardMarkup(
             [
                 [
-                    InlineKeyboardButton(text="open link", url=f"https://telegra.ph{response[0]}"),
-                    InlineKeyboardButton(text="share link", url=f"https://telegram.me/share/url?url=https://telegra.ph{response[0]}")
-                ],
+
                 [InlineKeyboardButton(text="✗ Close ✗", callback_data="close_data")]
             ]
         )
